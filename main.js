bb519e4f15c6a85cd5e6f98272ea0a91c322895f
OmerFarukYilmaz-github <66321088+OmerFarukYilmaz-github@users.noreply.github.com>
2022-10-19 19:09:26 +0300

    Add postMiddleware

--- main.js
+++ main.js
@@ -11,43 +11,49 @@ const helloPath = "/hello"
 app.use(cors());
 app.use(bodyParser.json());
 
-const middleware = function (req, res, next) 
+const globalMiddleware = function (req, res, next) 
 {
   console.log("Middleware got a new request");
  next();
 };
+const postSpecificMiddleware = function (req, res, next) 
+{
+  console.log("post Specific Middleware got a POST request");
+ next();
+};
 
+app.use(globalMiddleware);
 
-app.get("/", middleware, function (req, res) 
+app.get("/", function (req, res) 
 {
   console.log("Opening page");
   res.json("It'S a opening page");
 });
 
 
-app.get(helloPath, middleware, function (req, res) 
+app.get(helloPath, function (req, res) 
 {
   console.log("Get page");
-  res.json("It's a get request");
+  res.json("Hello! It's a get request");
 });
 
 
-app.post(helloPath, middleware,function (req, res) 
+app.post(helloPath, postSpecificMiddleware, function (req, res) 
 {
   console.log("Post page");
-  res.json("It's a post request");
+  res.json("Hello! It's a post request");
 });
 
-app.put(helloPath, middleware, function (req, res) 
+app.put(helloPath, function (req, res) 
 {
   console.log("Put page");
-  res.json("It's a put request");
+  res.json("Hello! It's a put request");
 });
 
-app.delete(helloPath, middleware, function (req, res) 
+app.delete(helloPath, function (req, res) 
 {
   console.log("Delete page");
-  res.json("It's a delete request");
+  res.json("Hello! It's a delete request");
 });
 
 app.listen(PORT, () => 
